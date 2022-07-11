6f07f441421c190dfe3a1729d6b05510cbac5cb3
Tiirth <tiirthdeora@gmail.com>
2022-07-11 10:02:40 +0530

    first

---
+++ components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Portfolio from './Portfolio'
+import Promos from './Promos'
+
+const Main = ({ thirdWebTokens, sanityTokens, walletAddress }) => {
+  return (
+    <Wrapper>
+      <Portfolio walletAddress={walletAddress} sanityTokens={sanityTokens} thirdWebTokens={thirdWebTokens} />
+      <Promos />
+    </Wrapper>
+  )
+}
+
+export default Main
+
+const Wrapper = styled.div`
+  display: flex;
+  & div {
+    border-radius: 0.4rem;
+  }
+`
